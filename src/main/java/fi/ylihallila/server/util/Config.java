3f75d8ce881200f28d6c2de7fecdfdb4a1b1d7da
Aaron Yli-Hallila <aaron.yli-hallila@student.oulu.fi>
2021-01-10 18:05:43 +0200

    Copy reference config to server directory

--- src/main/java/fi/ylihallila/server/util/Config.java
+++ src/main/java/fi/ylihallila/server/util/Config.java
@@ -1,16 +1,34 @@
 package fi.ylihallila.server.util;
 
 import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Config {
 
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
+    private static final Path configPath = Path.of("application.conf");
+
+    // Copy reference config to server directory
+    static {
+        if (!(Files.exists(configPath))) {
+            try {
+                Files.copy(Config.class.getResourceAsStream("/reference.conf"), configPath);
+            } catch (IOException e) {
+                logger.error("Could not copy reference config", e);
+            }
+        }
+    }
+
     /**
-     * Because ConfigFactory.parseFile(...); does not include the reference.conf, we need to obtain
-     * the reference config with ConfigFactory.load(); and provide that as a fallback.
+     * Loads the reference config (resources/reference.conf)
      */
     private static final com.typesafe.config.Config baseConfig = ConfigFactory.load();
-    public static com.typesafe.config.Config Config = ConfigFactory.parseFile(new File("application.conf")).withFallback(baseConfig);
+
+    public static com.typesafe.config.Config Config = ConfigFactory.parseFile(configPath.toFile()).withFallback(baseConfig);
 
 }
